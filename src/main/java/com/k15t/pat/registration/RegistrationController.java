96d68286e9cd4860f0a0f02ffaa85fdf84412687
Ahmed Wahbi <ahmedozy@gmail.com>
2019-11-14 16:50:54 +0200

    1- remove velocity 2- add thymeleaf 3- upgrade spring boot version

--- src/main/java/com/k15t/pat/registration/RegistrationController.java
+++ src/main/java/com/k15t/pat/registration/RegistrationController.java
@@ -1,29 +1,20 @@
 package com.k15t.pat.registration;
 
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.StringWriter;
 
 
-@RestController
+@Controller
 public class RegistrationController {
 
-    @Autowired private VelocityEngine velocityEngine;
 
 
-    @RequestMapping("/registration.html")
+    @RequestMapping("/registration")
     public String registration() {
-
-        Template template = velocityEngine.getTemplate("templates/registration.vm");
-        VelocityContext context = new VelocityContext();
-        StringWriter writer = new StringWriter();
-        template.merge(context, writer);
-
-        return writer.toString();
+		return null;
     }
 }
