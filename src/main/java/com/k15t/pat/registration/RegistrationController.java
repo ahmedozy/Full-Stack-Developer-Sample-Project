d6a3f38a9fcc992d677c845619f3e4ecae81d9da
Ahmed Wahbi <ahmedozy@gmail.com>
2019-11-14 18:57:51 +0200

    use thymeleaf to render the page

--- src/main/java/com/k15t/pat/registration/RegistrationController.java
+++ src/main/java/com/k15t/pat/registration/RegistrationController.java
@@ -3,6 +3,7 @@ package com.k15t.pat.registration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.StringWriter;
@@ -13,8 +14,8 @@ public class RegistrationController {
 
 
 
-    @RequestMapping("/registration")
+    @GetMapping("/registration")
     public String registration() {
-		return null;
+		return "registration";
     }
 }
